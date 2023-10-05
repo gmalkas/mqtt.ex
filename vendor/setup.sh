d3df3a5b9842b5445ac8998a8393b96199b9e05c
Gabriel Malkas <gabriel.malkas@gmail.com>
2023-10-05 11:39:34 -0600

    Add script to setup VerneMQ for integration tests

---
+++ vendor/setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd vernemq
+make rel
+cd _build/default/rel/vernemq
+
+echo "Changing configuration to allow anonymous and SSL..."
+mkdir -p etc/conf.d
+cat << EOF > etc/conf.d/mqttex.conf
+allow_anonymous = on
+listener.ssl.name = 127.0.0.1:8883
+listener.ssl.certfile = ./etc/cert.pem
+listener.ssl.keyfile = ./etc/key.pem
+EOF
+
+echo "Adding user entry for 'mqttex_basic'..."
+echo "mqttex_basic:password" > etc/vmq.passwd
+./bin/vmq-passwd -U etc/vmq.passwd
+
+echo "Creating self-signed TLS certificate..."
+openssl req -x509 -out etc/cert.pem -keyout etc/key.pem \
+  -newkey rsa:2048 -nodes -sha256 \
+  -subj '/CN=localhost' -extensions EXT -config <( \
+  printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
